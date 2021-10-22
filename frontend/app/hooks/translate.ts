1c3b352c3b5b240c1e37cb0bcdd0a68b702bd276
Thanh Vu <csnsjh@gmail.com>
2021-10-23 00:08:05 +0700

    chore: apply eslint's dependencies

--- frontend/app/hooks/translate.ts
+++ frontend/app/hooks/translate.ts
@@ -3,14 +3,14 @@ import jp from '@translations/jp.json';
 import vi from '@translations/vi.json';
 import { useRouter } from 'next/router';
 
-interface Lang {
+interface Language {
   [key: string]: string;
 }
 
 export const useTranslate = () => {
   const router = useRouter();
   const { locale } = router;
-  let language: { [key: string]: string } = en;
+  let language: Language = en;
   if (locale === 'vi') language = vi;
   if (locale === 'jp') language = jp;
 
